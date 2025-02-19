a6703840b4b829d28083b4b7f16f2031977a7ae4
dracu-lah <nevilnicks4321@gmail.com>
2025-02-19 12:06:38 +0530

    updated swagger

--- src/swagger.js
+++ src/swagger.js
@@ -31,5 +31,5 @@ const options = {
 const specs = swaggerJsdoc(options);
 
 module.exports = (app) => {
-  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
+  app.use("/", swaggerUi.serve, swaggerUi.setup(specs));
 };
